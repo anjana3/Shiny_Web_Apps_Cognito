99f5d16c6e94f3568fb7a3cfc270648c5f6b6ab4
Anjana Narayana.K <devi.anjana5@gmail.com>
2023-03-26 11:11:15 +0530

    login page

---
+++ R/login_page.R
@@ -0,0 +1,12 @@
+login_page_UI<-function(id){
+  ns<-NS(id)
+}
+
+
+login_page_Server<-function(id,dataset,input_value){
+
+  moduleServer(id, function(input, output, session) {
+
+    vals <- reactiveValues()
+  })
+}
